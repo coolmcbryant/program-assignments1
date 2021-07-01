4f58ae1ce570b38cfc057dba3451721aab0ad55c
coolmcbryant git config --global user.email <you@example.com>
2021-07-01 00:54:11 -0700

    second commit

--- lab3.py
+++ lab3.py
@@ -1,10 +1,19 @@
-lab3.py
+Basketball_players = {"Lebron": 23, "Kevin durant ": 7, "Kobe bryant": 24, "Stephen Curry": 30,}
+cities = ["Oakland", "Atlanta", "New York City","Seattle", "Memphis", "Miami", "Boston", "Los Angeles", "Denver", "New Orleans"]
+Basketball_teams = ["Lakers", "Spurs", "warriors", "brooklyn", "knicks", "celtics", "hawks", "clippers", "bucks, "]toronto raptors
+Lebron_position = Basketball_players["Lebron"] + 2'
+Lebron_position = Basketball_players["Lebron"] =2'
+print(Lebron_position)
+list1 = cities[0:3]
+list2 = Basketball_teams[0:3]
+Coprint(Combined_list)
+print(Combined_list)
+cities[0] = "San Francisco"
+cities[2] = "Brooklyn"
+cities[7] = "Hollywood"
+cities[5] = "Tampa"
+print(cities)
+cities.extend(["New York City", "Los Angeles"])
+cities.insert(0, "Miami")
 
-
-
-for i in range(2,100):
-    for j in range(2,i):
-        if i%j == 0:
-            break
-        else:
-            print(i)
+print(cities)
